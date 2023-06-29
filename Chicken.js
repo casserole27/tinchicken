68706cfc68bf389e54960c446078637f6b5878b1
C Lewis <casserolecodes@gmail.com>
2023-06-29 19:56:43 -0400

    added new methods to Chicken class

--- Chicken.js
+++ Chicken.js
@@ -7,29 +7,46 @@ class Chicken {
         const {name, avatar, alt, age, bio} = this;
 
         return `
-        <img src="images/badge-like.png" 
-            alt=""
-            class="badge"
-            id="like-badge">
-        <img src="images/badge-nope.png" 
-            alt=""
-            class="badge"
-            id="nope-badge">    
         <img src="${avatar}" 
-            alt="${alt}"
-            class="chicken-img">   
-    
+        alt="${alt}"
+        class="chicken-img">   
+
         <div class="txt-container">    
         <h1 class="title">${name},
             <span class="age">${age}
         </h1>
         <h2 class="subtitle">${bio}</h2>
-        </div>  
+        </div>`
+
+    };    
+
+    swipeChicken() {
+        this.hasBeenSwiped = true;
+    }
+    
+    likeChicken() {
+        this.hasBeenLiked = true;
+    }
+
+    likeBadge() {
+        return `
+        <img src="images/badge-like.png" 
+        alt=""
+        class="badge"
+        id="like-badge">
         `
-    };
+    }
 
+    nopeBadge() {
+        return `
+        <img src="images/badge-nope.png" 
+        alt=""
+        class="badge"
+        id="nope-badge"> 
+        `
+    }
 
-};
+ };
 
 export default Chicken
 
